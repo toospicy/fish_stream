151fced24508976742d8f89bb13016fbca68bd95
joe <joe@joes-MacBook-Pro.local>
2024-12-16 01:01:54 -0800

    initial commit of fish stream project

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+video_capture = cv2.VideoCapture(0)  # Use the default webcam
+
+def generate_frames():
+    while True:
+        success, frame = video_capture.read()
+        if not success:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
